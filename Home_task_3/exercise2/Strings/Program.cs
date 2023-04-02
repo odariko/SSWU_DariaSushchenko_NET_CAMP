059ce9ec71651c048f45ef8b27b79345ca0c0de4
Daria Sushchenko <daria.gvendoline@gmail.com>
2023-04-02 21:54:37 +0300

    add Home_task_3

---
+++ Home_task_3/exercise2/Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringsWork stringsWork = new StringsWork();
+            string text = "I am a lazzy frog am I";
+            string substring = "am";
+            string replacement = "productive";
+
+            Console.WriteLine("Second substring in " + stringsWork.FindSecondSubstringIndex(text, substring));
+            Console.WriteLine("Found capitalized words " + stringsWork.CountCapitalizedWords(text));
+            Console.WriteLine(stringsWork.ReplaceWordsWithDoubledLetters(text, replacement));
+        }
+    }
+}
---
+++ Home_task_3/exercise2/Strings/StringsWork.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class StringsWork
+    {
+        public int? FindSecondSubstringIndex(string text, string substring)
+        {
+            int firstIndex = text.IndexOf(substring);
+            if (firstIndex == -1)
+            {
+                return null;
+            }
+
+            int secondIndex = text.IndexOf(substring, firstIndex + 1);
+            if (secondIndex == -1)
+            {
+                return null;
+            }
+
+            return secondIndex;
+        }
+
+        public int CountCapitalizedWords(string text)
+        {
+            int count = 0;
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(word) && char.IsUpper(word[0]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public string ReplaceWordsWithDoubledLetters(string text, string replacement)
+        {
+            string[] words = text.Split(' ');
+            string word = words[0];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                word = words[i];
+
+                for (int j = 0; j < word.Length - 1; j++)
+                {
+                    if (word[j] == word[j + 1])
+                    {
+                        words[i] = replacement;
+                        break;
+                    }
+                }
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
