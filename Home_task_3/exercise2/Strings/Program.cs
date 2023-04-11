31649ec8bb68f5bd616b762384900bc5c22b8184
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-11 15:25:20 +0300

    Update Program.cs

--- Home_task_3/exercise2/Strings/Program.cs
+++ Home_task_3/exercise2/Strings/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace Strings
-{
+{// 1 задача.Симулятор має помпу, має usera і їх створює. тому мала бути композиція з зафарбованими  ромбиками до симулятора
     internal class Program
     {
         static void Main(string[] args)
         {
             StringsWork stringsWork = new StringsWork();
+            // тут ще одне словечко міняти треба
             string text = "I am a lazzy frog am I";
             string substring = "am";
             string replacement = "productive";
